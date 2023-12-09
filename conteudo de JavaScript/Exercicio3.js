483d8ea70785051cfa7df55b0a1f0897ea4924c7
Zacher99 <matheus.zacher@outlook.com>
2023-12-09 11:44:04 -0300

    Modificado exercicio3

--- conteudo de JavaScript/Exercicio3.js
+++ conteudo de JavaScript/Exercicio3.js
@@ -1,19 +1,29 @@
-const altura = 1.80
-const peso = 80
-let imc = peso / (altura * altura)
+function calculoImc(peso, altura) {
+    return peso / Math.pow(altura, 2);
+}
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return 'Abaixo do peso'
+    }
+    else if (imc <= 25) {
+        return 'Peso normal'
+    }
+    else if (imc <= 30) {
+        return 'Acima do peso'
+    }
+    else if (imc <= 40) {
+        return 'Obeso'
+    }
+    else {
+        return 'Obesidade Grave'
+    }
+}
 
-if (imc < 18.5) {
-    console.log('Abaixo do peso')
+
+function main(){
+    const peso = 50;
+    const altura = 1.80;
+    const imc = calculoImc(peso, altura);
+    console.log(classificarImc(imc))
 }
-else if (imc <= 25) {
-    console.log('Peso normal')
-}
-else if (imc <= 30) {
-    console.log('Acima do peso')
-}
-else if (imc <= 40) {
-    console.log('Obeso')
-}
-else {
-    console.log('Obesidade Grave')
-}
+main()    
